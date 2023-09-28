226b46a143d1308af0ce3f233a16cb727e695dda
MaheshBBhor <maheshbhor97@gmail.com>
2023-09-28 21:27:21 +0530

    hf

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Mahesh Bhor
         </a>
       </header>
     </div>
