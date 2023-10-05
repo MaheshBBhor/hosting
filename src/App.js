1afe65b0cccb4d3001eefe96c3998bd25c0e11bf
MaheshBBhor <maheshbhor97@gmail.com>
2023-10-05 18:37:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from "./components/Login";
-import Signup from "./components/Signup";
+// import Signup from "./components/Signup";
 import Message from "./components/Message";
 import React, { useState } from "react";
 
