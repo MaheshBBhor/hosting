155bbdec414f5e9bbf24a110c817c19f9d7a40ba
MaheshBBhor <maheshbhor97@gmail.com>
2023-10-05 17:45:21 +0530

    adding

---
+++ src/components/Dairy/DiaryForm.js
@@ -0,0 +1,38 @@
+// src/components/DiaryForm.js
+import React, { useState } from 'react';
+
+function DiaryForm({ addEntry }) {
+  const [date, setDate] = useState('');
+  const [productName, setProductName] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [fat, setFat] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const entry = { date, productName, quantity, fat };
+    addEntry(entry);
+    setDate('');
+    setProductName('');
+    setQuantity('');
+    setFat('');
+  };
+
+  return (
+    <div className="diary-form">
+      <h2>Add Diary Entry</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Date:</label>
+        <input type="date" value={date} onChange={(e) => setDate(e.target.value)} required />
+        <label>Product Name:</label>
+        <input type="text" value={productName} onChange={(e) => setProductName(e.target.value)} required />
+        <label>Quantity:</label>
+        <input type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)} required />
+        <label>Fat:</label>
+        <input type="number" value={fat} onChange={(e) => setFat(e.target.value)} required />
+        <button type="submit">Add Entry</button>
+      </form>
+    </div>
+  );
+}
+
+export default DiaryForm;
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,18 @@
 // src/components/MessageToSonali.js
 import React, { useEffect, useState } from "react";
 import './diary.css';
+import DiaryEntry from './Dairy/DiaryEntry';
+import DiaryForm from './Dairy/DiaryForm';
 
 function Message() {
   const [dairyData,setDairyData]=useState([])
   const [brandName, setBrandName] = useState("Dwarkadhish Milk Dairy");
+  const [entries, setEntries] = useState([]);
 
+  const addEntry = (entry) => {
+    setEntries([...entries, entry]);
+    // You can also save the data to a JSON file here
+  };
 
   useEffect(()=>{
     const interval=setInterval(()=>{
@@ -24,7 +31,18 @@ function Message() {
     <div>
       <h2>Hi Mahesh</h2>
       <h1 className="brandName">{brandName}</h1>
-     
+      <DiaryForm addEntry={addEntry} />
+      <div className="diary-entries">
+        {entries.map((entry, index) => (
+          <DiaryEntry
+            key={index}
+            date={entry.date}
+            productName={entry.productName}
+            quantity={entry.quantity}
+            fat={entry.fat}
+          />
+        ))}
+      </div>
     </div>
   );
 }
